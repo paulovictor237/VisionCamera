f136c2cd3b7a5eb3f2fead4cd19c5a730e49a88f
Paulo Victor Duarte <paulovictor237@gmail.com>
2023-12-21 10:24:17 -0300

    reanimated

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['nativewind/babel', 'react-native-worklets-core/plugin'],
+  plugins: [
+    'nativewind/babel',
+    'react-native-worklets-core/plugin',
+    'react-native-reanimated/plugin',
+  ],
 };
